33212adfc4d2546e5924d16de2ee7b083a1d3934
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-12 13:46:20 +1000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
 Airplane.destroy_all
-a1 = Airplane.create :name => 'VG100', :row => 31, :column => 8
-a2 = Airplane.create :name => 'QA100', :row => 29, :column => 4
+a1 = Airplane.create :name => 'BA100', :row => 31, :column => 8
+a2 = Airplane.create :name => 'BA282', :row => 29, :column => 4
+a3 = Airplane.create :name => 'BA780', :row => 45, :column => 8
+a4 = Airplane.create :name => 'BA622', :row => 25, :column => 4
 
 puts "#{ Airplane.count } airplanes"
 
 Flight.destroy_all
-f1 = Flight.create :date => "12/05/2021", :flight_to => "QLD", :flight_from => "SYD"
-f2 = Flight.create :date => "13/05/2021", :flight_to => "ACT", :flight_from => "VIC"
+f1 = Flight.create :date => "15/05/2021", :flight_to => "OOL", :flight_from => "SYD"
+f2 = Flight.create :date => "16/05/2021", :flight_to => "OOL", :flight_from => "SYD"
+f3 = Flight.create :date => "16/05/2021", :flight_to => "SYD", :flight_from => "MEL"
+f4 = Flight.create :date => "18/05/2021", :flight_to => "ADL", :flight_from => "SYD"
+f5 = Flight.create :date => "23/05/2021", :flight_to => "SYD", :flight_from => "ADL"
+f6 = Flight.create :date => "28/05/2021", :flight_to => "HBA", :flight_from => "SYD"
 
 puts "#{ Flight.count } flights"
 
@@ -21,8 +27,10 @@ puts "#{ User.count } users"
 
 puts "Airplane and Flights"
 a1.flights << f1 << f2
-a2.flights << f1
+a2.flights << f3
+a3.flights << f4 << f5
+a4.flights << f6
 
 puts "Users and Flights"
-u1.flights << f1 << f2
+u1.flights << f4 << f5
 f1.users << u2 << u3
